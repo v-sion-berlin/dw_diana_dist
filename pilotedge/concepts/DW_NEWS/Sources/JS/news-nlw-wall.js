e6ccd18d52a7fc85f075cdfd8ac4fa2c64121182
Andreas Müller <andreas.mueller@v-sion.de>
2023-03-13 14:12:25 +0100

    Update 13.03.2023

--- pilotedge/concepts/DW_NEWS/Sources/JS/news-nlw-wall.js
+++ pilotedge/concepts/DW_NEWS/Sources/JS/news-nlw-wall.js
@@ -1,10 +1,12 @@
-/* global structuredClone */
+/* global Event, Option, structuredClone */
 /* global nlw */
 /* eslint-disable no-unused-vars */
 
 const NLW_PROPERTIES = Object.freeze({
   STUDIO: 0
 })
+
+const IS_DEFAULT = 'X'
 
 const initializeNLWData = (table) => {
   const NLW = nlw.data
@@ -52,4 +54,43 @@ const getFilteredData = (data, query) => {
       return typeof settingValue === 'string' ? settingValue.includes(queryValue) : settingValue === queryValue
     })
   })
+}
+
+const settingSelectChanged = (event, areaSelect, nlwData) => {
+  const Setting = event.target.value
+  if (event.detail !== 'dw.js' && Setting) {
+    // Clear area select
+    while (areaSelect.length > 1) {
+      areaSelect.remove(areaSelect.length - 1)
+    }
+
+    // Get new areas
+    const data = getFilteredData(nlwData, { Setting })
+    const areas = getDistinctSettingValues(data, 'Area')
+    let defaultSetting = getFilteredData(data, { IsDefault: IS_DEFAULT })
+    // if there is no default setting defined we use the first setting
+    defaultSetting = defaultSetting[0] ? defaultSetting : [data[0]]
+
+    // Fill area select
+    for (const area of areas) {
+      areaSelect.add(new Option(area))
+    }
+
+    // If we changed the setting manually, we are setting the default area
+    if (event.detail !== 'vizPayloadReady' && defaultSetting[0].Area) {
+      window.COElement('Dummy/Area').value = defaultSetting[0].Area
+      areaSelect.dispatchEvent(new Event('change'))
+    } else if (window.COElement('Dummy/Area').getFieldText()) {
+      areaSelect.value = window.COElement('Dummy/Area').getFieldText()
+    }
+  }
+}
+
+const areaSelectChanged = (event, nlwData) => {
+  if (event.detail !== 'dw.js') {
+    // Get new position and width
+    const Area = event.target.value
+    const [{ Setting }] = getFilteredData(nlwData, { Area })
+    window.COElement('PlayoutAreaKey').value = `${Setting} | ${Area}`
+  }
 }
